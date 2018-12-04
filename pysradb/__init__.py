e94171146b27a098e89f8955cdd35b9a7eaf8748
Saket Choudhary <saketkc@gmail.com>
2018-12-03 21:11:05 -0800

    Bumpversion 0.2.0 -> 0.2.2

--- pysradb/__init__.py
+++ pysradb/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Saket Choudhary"""
 __email__ = 'saketkc@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.2'
 
 from .pysradb import download_sradb_file
 from .pysradb import SRAdb
