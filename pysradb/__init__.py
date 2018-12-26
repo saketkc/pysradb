14318c644d8124f8208d6ffe5b3448ebb7a63e86
Saket Choudhary <saketkc@gmail.com>
2018-12-25 16:01:10 -0800

    Bump version: 0.5.0 → 0.6.0

--- pysradb/__init__.py
+++ pysradb/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Saket Choudhary"""
 __email__ = 'saketkc@gmail.com'
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 from .sradb import download_sradb_file
 from .sradb import SRAdb
--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/saketkc/pysradb',
-    version='0.5.0',
+    version='0.6.0',
     zip_safe=False,
 )
