380c85ce1c9384a18bce429d54c3de5bee4389da
Saket Choudhary <saketkc@gmail.com>
2019-01-15 21:52:48 -0800

    More formatting changes

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 import pysradb
 
@@ -33,27 +34,32 @@ import pysradb
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting', 'sphinx.ext.mathjax',
-    'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.doctest',
-    'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram', 'numpydoc',
-    'sphinxcontrib.fulltoc'
+    "IPython.sphinxext.ipython_directive",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.inheritance_diagram",
+    "numpydoc",
+    "sphinxcontrib.fulltoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'pysradb'
+project = u"pysradb"
 copyright = u"2018, Saket Choudhary"
 author = u"Saket Choudhary"
 # The version info for the project you're documenting, acts as replacement
@@ -75,10 +81,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -89,7 +95,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -100,13 +106,13 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pysradbdoc'
+htmlhelp_basename = "pysradbdoc"
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -115,15 +121,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -133,9 +136,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'pysradb.tex',
-     u'pysradb Documentation',
-     u'Saket Choudhary', 'manual'),
+    (master_doc, "pysradb.tex", u"pysradb Documentation", u"Saket Choudhary", "manual")
 ]
 
 
@@ -143,11 +144,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'pysradb',
-     u'pysradb Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "pysradb", u"pysradb Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------
@@ -156,14 +153,16 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'pysradb',
-     u'pysradb Documentation',
-     author,
-     'pysradb',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "pysradb",
+        u"pysradb Documentation",
+        author,
+        "pysradb",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
-
 
 
 numpydoc_show_class_members = False
@@ -171,7 +170,7 @@ numpydoc_show_class_members = False
 import guzzle_sphinx_theme
 
 html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
+html_theme = "guzzle_sphinx_theme"
 
 # Register the theme as an extension to generate a sitemap.xml
 extensions.append("guzzle_sphinx_theme")
@@ -179,9 +178,8 @@ extensions.append("guzzle_sphinx_theme")
 # Guzzle theme options (see theme.conf for more information)
 html_theme_options = {
     # Set the name of the project to appear in the sidebar
-    "project_nav_name": "pysradb",
+    "project_nav_name": "pysradb"
 }
 
 scv_greatest_tag = True
 scv_show_banner = True
-
--- setup.py
+++ setup.py
@@ -4,48 +4,45 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['pandas>=0.23.4', 'tqdm>=4.28', 'click>=7.0']
-test_requirements = [
-    'pytest',
-]
+requirements = ["pandas>=0.23.4", "tqdm>=4.28", "click>=7.0"]
+test_requirements = ["pytest"]
 
 setup(
-    author='Saket Choudhary',
-    author_email='saketkc@gmail.com',
+    author="Saket Choudhary",
+    author_email="saketkc@gmail.com",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Healthcare Industry',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Healthcare Industry",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
-    description=
-    "Python package for interacting with SRAdb and downloading datasets from SRA",
-    entry_points={'console_scripts': ['pysradb=pysradb.cli:cli']},
+    description="Python package for interacting with SRAdb and downloading datasets from SRA",
+    entry_points={"console_scripts": ["pysradb=pysradb.cli:cli"]},
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='pysradb',
-    name='pysradb',
-    packages=['pysradb'],
+    keywords="pysradb",
+    name="pysradb",
+    packages=["pysradb"],
     setup_requires=requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/saketkc/pysradb',
-    version='0.6.0',
+    url="https://github.com/saketkc/pysradb",
+    version="0.6.0",
     zip_safe=False,
 )
