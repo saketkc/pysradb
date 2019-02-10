5bb71bc0c78a7705c64947aa0f0736bac8f79027
Saket Choudhary <saketkc@gmail.com>
2019-02-09 18:38:50 -0800

    Fix version in setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/saketkc/pysradb",
-    version="version='0.7.0'",
+    version="0.7.0",
     zip_safe=False,
 )
