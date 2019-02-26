15dde978c9322c0b89d2900caa69460b0c6c0802
Saket Choudhary <saketkc@gmail.com>
2019-02-26 11:21:12 -0800

    Bump version: 0.8.0-dev0 → 0.8.0

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/saketkc/pysradb",
-    version="0.8.0-dev0",
+    version="0.8.0",
     zip_safe=False,
 )
