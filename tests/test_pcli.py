a28dd749e3029a4c09ab8e03c2db3de09e3df53a
Saket Choudhary <saketkc@gmail.com>
2019-02-27 17:27:23 -0800

    Handle < py3.7 in test cases

--- tests/test_pcli.py
+++ tests/test_pcli.py
@@ -8,6 +8,15 @@ import pytest
 from shlex import quote
 from shlex import split
 import subprocess
+import sys
+
+
+def run(command):
+    if sys.version_info.minor >= 7:
+        result = subprocess.run(split(command), capture_output=True)
+    else:
+        result = subprocess.run(split(command), check=True, stdout=subprocess.PIPE)
+    return str(result.stdout).strip()
 
 
 @pytest.fixture(scope="module")
@@ -22,145 +31,77 @@ def test_all_row_counts_sra(sradb_connection):
 
 
 def test_download():
-    result = subprocess.run(
-        [
-            "pysradb",
-            "download",
-            "-y",
-            "--db",
-            "data/SRAmetadb.sqlite",
-            "--out-dir",
-            "srp_downloads",
-            "-p",
-            "SRP063852",
-        ],
-        capture_output=True,
+    result = run(
+        "pysradb download -y --db data/SRAmetadb.sqlite --out-dir srp_downloads -p SRP063852"
     )
-    assert "SRP063852" in str(result.stdout)
+    assert "SRP063852" in result
     assert os.path.getsize("srp_downloads/SRP063852/SRX1254413/SRR2433794.sra")
 
 
 def test_sra_metadata():
-    result = subprocess.run(
-        ["pysradb", "metadata", "SRP098789", "--db", "data/SRAmetadb.sqlite"],
-        capture_output=True,
-    )
-    assert "SRX2536403" in str(result.stdout)
+    result = run("pysradb metadata SRP098789 --db data/SRAmetadb.sqlite")
+    assert "SRX2536403" in result
 
 
 def test_sra_metadata():
-    result = subprocess.run(
-        [
-            "pysradb",
-            "metadata",
-            "SRP098789",
-            "--db",
-            "data/SRAmetadb.sqlite",
-            "--detailed",
-            "--expand",
-        ],
-        capture_output=True,
+    result = run(
+        "pysradb metadata SRP098789 --db data/SRAmetadb.sqlite --detailed --expand"
     )
-    assert "treatment_time" in str(result.stdout)
+    assert "treatment_time" in result
 
 
 def test_srp_to_srx():
-    result = subprocess.run(
-        ["pysradb", "srp-to-srx", "SRP098789", "--db", "data/SRAmetadb.sqlite"],
-        capture_output=True,
-    )
-    assert "SRX2536403" in str(result.stdout)
+    result = run("pysradb srp-to-srx SRP098789 --db data/SRAmetadb.sqlite")
+    assert "SRX2536403" in result
 
 
 def test_srp_assay():
-    result = subprocess.run(
-        [
-            "pysradb",
-            "metadata",
-            "SRP098789",
-            "--db",
-            "data/SRAmetadb.sqlite",
-            "--assay",
-        ],
-        capture_output=True,
-    )
-    assert "RNA-Seq" in str(result.stdout)
+    result = run("pysradb metadata SRP098789 --db data/SRAmetadb.sqlite --assay")
+    assert "RNA-Seq" in result
 
 
 def srr_to_srx():
-    result = subprocess.run(
-        [
-            "pysradb",
-            "srr-to-srx",
-            "--db",
-            "data/SRAmetadb.sqlite",
-            "SRR5227288",
-            "SRR649752",
-            "--desc",
-        ],
-        capture_output=True,
+    result = run(
+        "pysradb srr-to-srx --db data/SRAmetadb.sqlite SRR5227288 SRR649752 --desc"
     )
-    assert "3T3 cells" in str(result.stdout)
+    assert "3T3 cells" in result
 
 
 def srx_to_srr():
-    result = subprocess.run(
-        [
-            "pysradb",
-            "srr-to-srx",
-            "--db",
-            "data/SRAmetadb.sqlite",
-            "SRX217956",
-            "SRX2536403",
-            "--desc",
-        ],
-        capture_output=True,
+    result = run(
+        "pysradb srr-to-srx --db data/SRAmetadb.sqlite SRX217956 SRX2536403 --desc"
     )
-    assert "3T3 cells" in str(result.stdout)
+    assert "3T3 cells" in result
 
 
 def test_sra_metadata_detail():
-    result = subprocess.run(
-        split(
-            "pysradb metadata --db data/SRAmetadb.sqlite SRP075720 --detailed --expand"
-        ),
-        capture_output=True,
+    result = run(
+        "pysradb metadata --db data/SRAmetadb.sqlite SRP075720 --detailed --expand"
     )
-    assert "retina" in str(result.stdout)
+    assert "retina" in result
 
 
 def test_srp_to_gse():
-    result = subprocess.run(
-        split("pysradb srp-to-gse --db data/SRAmetadb.sqlite SRP075720"),
-        capture_output=True,
-    )
-    assert "GSE81903" in str(result.stdout)
+    result = run("pysradb srp-to-gse --db data/SRAmetadb.sqlite SRP075720")
+    assert "GSE81903" in result
 
 
 def test_gsm_to_srp():
-    result = subprocess.run(
-        split("pysradb gsm-to-srp --db data/SRAmetadb.sqlite GSM2177186"),
-        capture_output=True,
-    )
-    assert "SRP075720" in str(result.stdout)
+    result = run("pysradb gsm-to-srp --db data/SRAmetadb.sqlite GSM2177186")
+    assert "SRP075720" in result
 
 
 def test_gsm_to_gse():
-    result = subprocess.run(
-        split("pysradb gsm-to-gse --db data/SRAmetadb.sqlite GSM2177186"),
-        capture_output=True,
-    )
-    assert "GSE81903" in str(result.stdout)
+    result = run("pysradb gsm-to-gse --db data/SRAmetadb.sqlite GSM2177186")
+    assert "GSE81903" in result
 
 
 def test_gsm_to_srr():
-    result = subprocess.run(
-        split(
-            "pysradb gsm-to-srr --db data/SRAmetadb.sqlite GSM2177186 --detailed --desc --expand"
-        ),
-        capture_output=True,
+    result = run(
+        "pysradb gsm-to-srr --db data/SRAmetadb.sqlite GSM2177186 --detailed --desc --expand"
     )
-    assert "GSM2177186_r1" in str(result.stdout)
+    assert "GSM2177186_r1" in result
+
 
 """
 def test_assay_uniq():
