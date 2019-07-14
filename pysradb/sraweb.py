560f817a6783d87791bd03baae0b952eb2fed626
Saket Choudhary <saketkc@gmail.com>
2019-07-13 19:23:16 -0700

    Begin phasing out SRAdb.sqlite requirement

---
+++ pysradb/sraweb.py
@@ -0,0 +1,193 @@
+from collections import OrderedDict
+
+import pandas as pd
+import requests
+
+
+class SRAweb(object):
+    def __init__(self):
+        self.base_url = {}
+        self.base_url[
+            "esummary"
+        ] = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
+        self.base_url[
+            "esearch"
+        ] = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+        self.esearch_params = {}
+        self.esearch_params["sra"] = [
+            ("db", "sra"),
+            ("usehistory", "y"),
+            ("retmode", "json"),
+        ]
+        self.esearch_params["geo"] = [
+            ("db", "gds"),
+            ("usehistory", "y"),
+            ("retmode", "json"),
+        ]
+
+    @classmethod
+    def format_xml(string):
+        """Create a fake root to make 'string' a valid xml
+
+        Parameters
+        ----------
+        string: str
+
+        Returns
+        --------
+        xml: str
+        """
+        string = unescape(string.strip())
+        return "<root>" + string + "</root>"
+
+    @classmethod
+    def xml_to_json(xml):
+        """Convert xml to json.
+
+        Parameters
+        ----------
+        xml: str
+             Input XML
+
+        Returns
+        -------
+        xml_dict: dict
+                  Parsed xml as dict
+        """
+        return xmltodict.parse(xml)["root"]
+
+    def create_esummary_params(self, esearchresult, db="sra"):
+        query_key = esearchresult["querykey"]
+        webenv = esearchresult["webenv"]
+        retstart = esearchresult["retstart"]
+        retmax = esearchresult["retmax"]
+        return [
+            ("query_key", query_key),
+            ("WebEnv", webenv),
+            ("retstart", retstart),
+            ("retmax", retmax),
+        ]
+
+    def get_esummary_reponse(self, db, term):
+
+        assert db in ["sra", "geo"]
+
+        payload = self.esearch_params[db]
+        payload += [("term", term)]
+        request = requests.get(self.base_url["esearch"], params=OrderedDict(payload))
+        response = request.json()
+        if "esummaryresult" in response:
+            print("No result found")
+            return
+
+        payload = self.esearch_params[db]
+        payload += self.create_esummary_params(response["esearchresult"])
+        request = requests.get(self.base_url["esummary"], params=OrderedDict(payload))
+
+        response = request.json()
+
+        if "esummaryresult" in response:
+            print("No result found")
+            return
+
+        return response["result"]
+
+    def sra_metadata(
+        self,
+        acc,
+        sample_attribute=False,
+        detailed=False,
+        expand_sample_attributes=False,
+        output_read_lengths=False,
+    ):
+
+        esummary_result = self.get_esummary_reponse("sra", acc)
+        uids = esummary_result["uids"]
+        exps_xml = OrderedDict()
+        runs_xml = OrderedDict()
+
+        exps_json = OrderedDict()
+        runs_json = OrderedDict()
+
+        for uid in uids:
+            exps_xml[uid] = format_xml(esummary_result[uid]["expxml"])
+            runs_xml[uid] = format_xml(esummary_result[uid]["runs"])
+
+        for uid in uids:
+            exps_json[uid] = xml_to_json(exps_xml[uid])
+            runs_json[uid] = xml_to_json(runs_xml[uid])
+
+        sra_record = []
+        for uid, run_json in runs_json.items():
+            exp_json = exps_json[uid]
+            runs = run_json["Run"]
+            if not isinstance(runs, list):
+                runs = [runs]
+            exp_title = exp_json["Summary"]["Title"]
+            exp_platform = exp_json["Summary"]["Platform"]
+            exp_total_runs = exp_json["Summary"]["Statistics"]["@total_runs"]
+            exp_total_total_spots = exp_json["Summary"]["Statistics"]["@total_spots"]
+            exp_total_total_size = exp_json["Summary"]["Statistics"]["@total_size"]
+
+            # experiment_accession
+            exp_ID = exp_json["Experiment"]["@acc"]
+            # experiment_title
+            exp_name = exp_json["Experiment"]["@name"]
+
+            exp_organism = exp_json["Organism"]
+            # organism_name
+            exp_organism_name = exp_organism["@ScientificName"]
+            # taxid
+            exp_taxid = exp_organism["@taxid"]
+
+            exp_sample = exp_json["Sample"]
+            # sample_accession
+            exp_sample_ID = exp_sample["@acc"]
+            # sample_title
+            exp_sample_name = exp_sample["@name"]
+
+            exp_library_descriptor = exp_json["Library_descriptor"]
+            # library_strategy
+            exp_library_strategy = exp_library_descriptor["LIBRARY_STRATEGY"]
+            if isinstance(exp_library_strategy, dict):
+                exp_library_strategy = exp_library_strategy["#text"]
+            # library_source
+            exp_library_source = exp_library_descriptor["LIBRARY_SOURCE"]
+            if isinstance(exp_library_source, dict):
+                exp_library_source = exp_library_source["#text"]
+            # library_selection
+            exp_library_selection = exp_library_descriptor["LIBRARY_SELECTION"]
+            if isinstance(exp_library_selection, dict):
+                exp_library_selection = exp_library_selection["#text"]
+            # library_layout
+            exp_library_layout = list(exp_library_descriptor["LIBRARY_LAYOUT"].keys())[
+                0
+            ]
+
+            experiment_record = OrderedDict()
+            experiment_record["experiment_accession"] = exp_ID
+            experiment_record["experiment_title"] = exp_name
+            experiment_record["experiment_desc"] = exp_title
+
+            experiment_record["organism_taxid "] = exp_taxid
+            experiment_record["organism_name"] = exp_organism_name
+
+            experiment_record["library_strategy"] = exp_library_strategy
+            experiment_record["library_source"] = exp_library_source
+            experiment_record["library_selection"] = exp_library_selection
+            experiment_record["library_source"] = exp_library_source
+
+            experiment_record["sample_accession"] = exp_sample_ID
+            experiment_record["sample_title"] = exp_sample_name
+
+            for run_record in runs:
+                run_accession = run_record["@acc"]
+                run_total_spots = run_record["@total_spots"]
+                run_total_bases = run_record["@total_bases"]
+
+                experiment_record["run_accession"] = run_accession
+                experiment_record["run_total_spots"] = run_total_spots
+                experiment_record["run_total_bases"] = run_total_bases
+
+                sra_record.append(experiment_record)
+        return pd.DataFrame(sra_record)
