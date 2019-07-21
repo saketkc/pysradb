c95c240889d920e63c85131c6571d9f9ddf757e1
Saket Choudhary <saketkc@gmail.com>
2019-07-21 00:41:31 -0700

    Bump version: 0.9.5 → 0.9.6-dev0

--- pysradb/__init__.py
+++ pysradb/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Saket Choudhary"""
 __email__ = "saketkc@gmail.com"
-__version__ = "0.9.5"
+__version__ = "0.9.6-dev0"
 
 from .sradb import download_sradb_file
 from .sradb import SRAdb
