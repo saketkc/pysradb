8d3571e2edc5889ef0546e0ad60543feb8215643
Saket Choudhary <saketkc@gmail.com>
2020-02-17 18:55:22 -0800

    Can now fetch fastq from ENA

--- pysradb/__init__.py
+++ pysradb/__init__.py
@@ -7,6 +7,7 @@ __version__ = "0.10.3-dev0"
 
 from .sradb import download_sradb_file
 from .sradb import SRAdb
+from .sraweb import SRAweb
 from .geodb import download_geodb_file
 from .geodb import GEOdb
 from .filter_attrs import expand_sample_attribute_columns
