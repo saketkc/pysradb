a5d9a684ac8c7ee1e19548495d25286a7105cc97
Saket Choudhary <saketkc@gmail.com>
2020-02-23 13:06:45 -0800

    Init SRAweb test

---
+++ tests/test_sraweb.py
@@ -0,0 +1,15 @@
+"""Tests for SRAweb"""
+
+import os
+import pytest
+from pysradb.sraweb import SRAweb
+
+@pytest.fixture(scope="module")
+def sraweb_connection():
+    db = SRAweb()
+    return db
+
+def test_sra_metadata(sraweb_connection):
+    df = sraweb_connection.sra_metadata("SRP016501")
+    assert df.shape[0] == 134
+
