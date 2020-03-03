fe34a7c9aa8978d1e7186280ff4eca8fdb29317f
Dibya Gautam <dibya.gautam7@gmail.com>
2020-03-04 00:01:39 +0530

    Tests for commands of the SRAweb module (#23)
    * Add test cases for SRAweb 
    * Introduced a fix to handle API rate limit exceeding errors
    
    Co-authored-by: Saket Choudhary <saketkc@gmail.com>

--- pysradb/sraweb.py
+++ pysradb/sraweb.py
@@ -204,6 +204,11 @@ class SRAweb(SRAdb):
         if "esummaryresult" in esearch_response:
             print("No result found")
             return
+        if "error" in esearch_response:
+            # API rate limite exceeded
+            esearch_response = _retry_response(
+                self.base_url["esearch"], payload, "esearchresult"
+            )
 
         n_records = int(esearch_response["esearchresult"]["count"])
 
@@ -251,7 +256,7 @@ class SRAweb(SRAdb):
         try:
             n_records = int(esearch_response["esearchresult"]["count"])
         except KeyError:
-            sys.stderr.write("Obtained esearch response: {}\n".format(esearch_response))
+            sys.stderr.write("obtained esearch response: {}\n".format(esearch_response))
             raise RuntimeError
 
         results = {}
