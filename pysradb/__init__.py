ded14216513068126e727f29b0538fc03aa5f1fc
Saket Choudhary <saketkc@gmail.com>
2022-01-10 11:30:15 -0500

    Bump version: 1.2.0-dev0 → 1.2.0

--- pysradb/__init__.py
+++ pysradb/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Saket Choudhary"""
 __email__ = "saketkc@gmail.com"
-__version__ = "1.2.0-dev0"
+__version__ = "1.2.0"
 
 from .filter_attrs import expand_sample_attribute_columns
 from .geodb import GEOdb
