1e9e2ccb082b2f05630c2ef4c12d059dfb523685
Saket Choudhary <saketkc@gmail.com>
2023-03-18 18:36:47 -0400

    Bump version: 2.0.1-dev0 → 2.0.1

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/saketkc/pysradb",
-    version="2.0.1-dev0",
+    version="2.0.1",
     zip_safe=False,
 )
