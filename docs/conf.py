3a9a5082ce27c1eb63466872f1de8f1c4272d7d1
Saket Choudhary <saketkc@gmail.com>
2023-06-08 12:37:11 -0400

    Update docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -64,8 +64,8 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-#source_suffix = ".md"
+source_suffix = [".rst", ".md"]
+# source_suffix = ".md"
 
 # The master toctree document.
 master_doc = "index"
